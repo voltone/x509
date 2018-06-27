6462449f5ceeabb47d510cc35c799b144b4f4d94
Bram Verburg <bram.verburg@radcom.com>
2018-06-27 10:36:35 +0300

    support OTP 20.0 and Elixir 1.5

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule X509.MixProject do
     [
       app: :x509,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
