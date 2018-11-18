5cac90b61317d50c6735c3912deb0f863c4cde40
Bram Verburg <bram.verburg@voltone.net>
2018-11-18 11:15:35 +0200

    Enable use of Logger in tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,11 @@
 ExUnit.configure(exclude: :openssl)
 ExUnit.start()
 
+# Logger is not included in the `extra_applications` list in the application
+# config in mix.exs, because it is not used at runtime. It is used in the
+# X509.Test.Server tests, so we need to start it explicitly.
+Application.ensure_all_started(:logger)
+
 defmodule TestHelper do
   # Returns the version of the specified OTP application as a list of integers
   def version(application) do
