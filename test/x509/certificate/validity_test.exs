b8986916c4b66111255aaa0dca8e10066730c5a6
Bram Verburg <bram.verburg@voltone.net>
2019-01-09 16:04:36 +0200

    Improve Validity encoding tests

--- test/x509/certificate/validity_test.exs
+++ test/x509/certificate/validity_test.exs
@@ -7,5 +7,8 @@ defmodule X509.Certificate.ValidityTest do
     {:ok, not_after, 0} = DateTime.from_iso8601("2051-12-31T23:59:59Z")
     validity = X509.Certificate.Validity.new(not_before, not_after)
     assert <<der::binary>> = :public_key.der_encode(:Validity, validity)
+
+    assert {:Validity, {:utcTime, '220101000000Z'}, {:generalTime, '20511231235959Z'}} =
+             :public_key.der_decode(:Validity, der)
   end
 end
