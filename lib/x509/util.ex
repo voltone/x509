00e4517d32fb9fd8d0d456024ec72ed3b2b9bf23
Thomas Cioppettini <544875+tomciopp@users.noreply.github.com>
2023-08-30 11:05:03 -0400

    Avoid Logger.warn deprecation working on recent Elixir versions (#66)

--- lib/x509/util.ex
+++ lib/x509/util.ex
@@ -1,6 +1,8 @@
 defmodule X509.Util do
   @moduledoc false
 
+  require Logger
+
   def app_version(application) do
     application
     |> Application.spec()
@@ -8,5 +10,17 @@ defmodule X509.Util do
     |> to_string()
     |> String.split(".")
     |> Enum.map(&String.to_integer/1)
+  end
+
+  # Create a utility function that handles checking for the 
+  # existence of Logger.warning/2 if not fallback to Logger.warn/2
+  if macro_exported?(Logger, :warning, 2) do
+    def warn(message, metadata \\ []) do
+      Logger.warning(message, metadata)
+    end
+  else
+    def warn(message, metadata \\ []) do
+      Logger.warn(message, metadata)
+    end
   end
 end
