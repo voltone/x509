e292275a8b20416e414a8472db62c5c6a9a73c01
Tomasz Kowal <tomekowal@gmail.com>
2024-01-12 17:30:48 +0000

    Don't warn on Logger.warn

---
+++ lib/x509/logger.ex
@@ -0,0 +1,16 @@
+defmodule X509.Logger do
+  @moduledoc false
+  alias X509.Util
+
+  require Logger
+
+  if Util.app_version(:elixir) >= [1, 11, 0] do
+    def warn(message, metadata \\ []) do
+      Logger.warning(message, metadata)
+    end
+  else
+    def warn(message, metadata \\ []) do
+      Logger.warn(message, metadata)
+    end
+  end
+end
